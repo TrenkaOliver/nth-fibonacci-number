9daa2ceb1344619c766500af1efafd297ef6662b
TrenkaOliver <156715125+TrenkaOliver@users.noreply.github.com>
2025-08-22 17:06:04 +0200

    Implement nth Fibonacci number calculation and input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's find the nth fibonacci number!");
+    println!("Enter n's value");
+
+    let mut n = String::new();
+    let n = loop {
+        io::stdin().read_line(&mut n).expect("Error reading the input");
+        match n.trim().parse::<isize>() {
+            Ok(num) => break num,
+            Err(_) => println!("Enter a whole number"),
+        };
+    };
+
+    let value = f(n);
+    if n < 0 && n % 2 == 0 {
+        println!("-{value}");
+    }
+    else {
+        println!("{value}");
+    }
+
+}
+
+fn f(n: isize) -> isize {
+    let n = n.abs();
+    if n < 2 {
+        return n;
+    } else {
+        let mut p2 = 0;
+        let mut p1 = 1;
+        let mut current = 0;
+
+        for _ in 2..=n {
+            current = p1 + p2;
+            p2 = p1;
+            p1 = current;
+        }
+
+        return current;
+    }
 }
